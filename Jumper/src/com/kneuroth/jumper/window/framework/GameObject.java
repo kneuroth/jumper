78141c61752b508335241195e7550eb1089d1400
kneuroth <34626694+kneuroth@users.noreply.github.com>
2019-02-20 21:01:33 -0500

    Made game object and object id
    Every object in the game will extend game object, each will have a unique id

---
+++ Jumper/src/com/kneuroth/jumper/window/framework/GameObject.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kneuroth.jumper.window.framework;
+
+import java.awt.Graphics;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Kelly
+ * Any kind of object within the game
+ */
+public abstract class GameObject {
+    
+    protected float x,y;
+    protected ObjectId id;
+    protected float velX = 0, velY = 0;
+    
+    public GameObject(float x, float y, ObjectId id){
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+    
+    public abstract void tick(LinkedList<GameObject> object);
+    
+    public abstract void render(Graphics g);
+    
+    public abstract float getX();
+    public abstract float getY();
+    public abstract float setX(float x);
+    public abstract float setY(float y);
+    
+    public abstract float getVelX();
+    public abstract float getVelY();
+    public abstract float setVelX(float x);
+    public abstract float setVelY(float y);
+    
+    
+}
