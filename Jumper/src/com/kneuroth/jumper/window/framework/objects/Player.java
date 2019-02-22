312d6717b3cff40aa5f0b6407e09ea49fd89b959
kneuroth <34626694+kneuroth@users.noreply.github.com>
2019-02-22 18:31:00 -0500

    Setup hitboxes

--- Jumper/src/com/kneuroth/jumper/window/framework/objects/Player.java
+++ Jumper/src/com/kneuroth/jumper/window/framework/objects/Player.java
@@ -9,6 +9,7 @@ import com.kneuroth.jumper.window.framework.GameObject;
 import com.kneuroth.jumper.window.framework.ObjectId;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.util.LinkedList;
 
@@ -18,7 +19,7 @@ import java.util.LinkedList;
  */
 public class Player extends GameObject{
     
-    private float width = 32, height = 64;
+    private float width = 48, height = 96;
     private float gravity = 0.5f;
     
     private final float MAX_SPEED = 10;
@@ -31,7 +32,7 @@ public class Player extends GameObject{
     @Override
     public void tick(LinkedList<GameObject> object) {
         x += velX;
-        y += velY;
+        //y += velY;
         
         //Gravity will affect it if falling or jumping
         if(falling || jumping){
@@ -41,18 +42,38 @@ public class Player extends GameObject{
             if(velY > MAX_SPEED){
                 velY = MAX_SPEED;
             }
-        }
+        }        
     }
 
     @Override
     public void render(Graphics g) {
+        
+        
+        
         g.setColor(Color.blue);
         g.fillRect((int)x, (int)y, (int)width, (int)height);
+        
+        Graphics2D g2d = (Graphics2D) g;
+        g.setColor(Color.red);
+        g2d.draw(getBounds());
+        g2d.draw(getBoundsRight());
+        g2d.draw(getBoundsLeft());
+        g2d.draw(getBoundsTop());
     }
 
     @Override
     public Rectangle getBounds() {
-        return new Rectangle((int)x, (int)y, (int)width, (int)height);
+        return new Rectangle((int) ((int)x + (width/2) - (width/2)/2), (int) ((int)y + (height/2)), (int)width/2, (int)height/2);
     }
+    public Rectangle getBoundsTop() {
+        return new Rectangle((int) ((int)x + (width/2) - (width/2)/2), (int)y, (int)width/2, (int)height/2);
+    }
+    public Rectangle getBoundsRight() {
+        return new Rectangle((int) ((int)x + width - 5), (int)y + 5, (int)5, (int)height - 10);
+    }
+    public Rectangle getBoundsLeft() {
+        return new Rectangle((int)x, (int)y + 5, (int)5, (int)height - 10);
+    }
+    
     
 }
